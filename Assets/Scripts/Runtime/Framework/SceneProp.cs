7503ba561a013479c6df32c0f7378dc6c0ea52a6
Minstreams <32974920+Minstreams@users.noreply.github.com>
2022-10-02 18:18:46 +0800

    Update

--- Assets/Scripts/Runtime/Framework/SceneProp.cs
+++ Assets/Scripts/Runtime/Framework/SceneProp.cs
@@ -7,15 +7,42 @@ namespace IceEngine
     [ExecuteInEditMode]
     public class SceneProp : MonoBehaviour
     {
-#if UNITY_EDITOR
+        public enum PropType
+        {
+            Brick,
+        }
+        public PropType type;
+        public Transform brick;
         protected virtual void Awake()
         {
+#if UNITY_EDITOR
             if (UnityEditor.EditorApplication.isPlaying) return;
             if (gameObject.scene.path == "") return;
 
             var funcGo = GameObject.Find("场景") ?? new GameObject("场景");
             transform.SetParent(funcGo.transform, true);
+#endif
+        }
+        protected virtual void Update()
+        {
+#if UNITY_EDITOR
+            switch (type)
+            {
+                case PropType.Brick:
+                    transform.position = transform.position.Snap();
+                    break;
+            }
+#endif
+        }
+#if UNITY_EDITOR
+        [Button("随机旋转")]
+        public void SetRandomRotation()
+        {
+            float RA() => Random.Range(0, 4) * 90;
+            brick.rotation = Quaternion.Euler(RA(), RA(), RA());
+            UnityEditor.EditorUtility.SetDirty(brick);
         }
 #endif
     }
+
 }
