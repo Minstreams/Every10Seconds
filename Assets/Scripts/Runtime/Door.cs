54fd0461b67161561ca74b43c632dabf8ec08200
Minstreams <32974920+Minstreams@users.noreply.github.com>
2022-10-03 19:44:03 +0800

    door

--- Assets/Scripts/Runtime/Door.cs
+++ Assets/Scripts/Runtime/Door.cs
@@ -8,14 +8,21 @@ namespace IceEngine
     public class Door : Pickable
     {
         public GameObject doorObj;
+        public GameObject doorMesh;
         public override void OnPick()
         {
             doorObj.SetActive(false);
+        }
+        protected override void OnPlayerEnter()
+        {
+            base.OnPlayerEnter();
+            doorMesh.layer = LayerMask.NameToLayer("PickableSelected");
         }
         protected override void OnPlayerExit()
         {
             base.OnPlayerExit();
             doorObj.SetActive(true);
+            doorMesh.layer = LayerMask.NameToLayer("Wall");
         }
     }
 }
--- Assets/Scripts/Runtime/Player.cs
+++ Assets/Scripts/Runtime/Player.cs
@@ -102,6 +102,8 @@ namespace IceEngine
 
         void Update()
         {
+            Shader.SetGlobalVector("PlayerPosition", transform.position);
+
             var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out var raycastHit, 100, aimMask))
             {
